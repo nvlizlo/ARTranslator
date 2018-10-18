4329bb022ae03881328051db0480ea21dbf64041
nvlizlo <>
2018-10-19 01:53:20 +0300

    Initial commit

---
+++ ARTranslator/ARTranslator/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  ARTranslator
+//
+//  Created by Nazariy Vlizlo on 10/19/18.
+//  Copyright © 2018 Nazariy Vlizlo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let ApiKey = "AIzaSyADpYYUQuBCOyjPJkoI2QD7NAXQZj3aAJE"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
